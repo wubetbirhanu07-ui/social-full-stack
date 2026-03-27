1114176f9e70250d35e60bcb0922c138a9008591
GECH <wubet@de11>
2026-03-27 12:20:31 -0400

    added env

--- frontend/src/clients/api.js
+++ frontend/src/clients/api.js
@@ -1,16 +1,17 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
 export const token = () => localStorage.getItem('token')
 
 export const userClient = axios.create({
-     baseURL: 'http://localhost:3000/api/users',
+     baseURL: BASE_URL + '/api/users',
      headers: {
           Authorization: `Bearer ${token()}`
      }
 })
 
 export const postClient = axios.create({
-     baseURL: 'http://localhost:3000/api/posts'
+     baseURL: `${BASE_URL}/api/posts`
 })
 
 // use the latest version of the token in local storage
